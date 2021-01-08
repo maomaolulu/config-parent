8abda4e7b9db7c4f31610fab4abe3900bcf8c6a5
maoly <maoly@ngarihealth.com>
2021-01-08 11:22:07 +0800

    config-server

---
+++ config-server/src/test/java/com/ngari/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngari.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
