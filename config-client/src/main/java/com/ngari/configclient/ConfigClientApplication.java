1fa20c1383a0cf873039661143d25a8e5eebc317
maoly <maoly@ngarihealth.com>
2021-01-18 15:00:19 +0800

    config高可用

--- config-client/src/main/java/com/ngari/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/ngari/configclient/ConfigClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -32,5 +33,8 @@ public class ConfigClientApplication {
 	public String hello(){
 		return democonfigclient;
 	}
+
+
+
 
 }
---
+++ eureka-server-config/src/test/java/com/example/eurekaserverconfig/EurekaServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserverconfig;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
