6dbb334ea6ca8cc037cc9236cc773490e1ea41d7
maoly <maoly@ngarihealth.com>
2021-01-19 16:09:23 +0800

    消息总线bus

--- config-client/src/main/java/com/ngari/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/ngari/configclient/ConfigClientApplication.java
@@ -3,12 +3,18 @@ package com.ngari.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
@@ -18,21 +24,12 @@ public class ConfigClientApplication {
 	@Value("${foo}")
 	String foo;
 
-	@Value("${democonfigclient.message}")
-	String democonfigclient;
-
-
-
 	@RequestMapping(value = "/hi")
 	public String hi(){
 		return foo;
 	}
 
 
-	@RequestMapping(value = "/hello")
-	public String hello(){
-		return democonfigclient;
-	}
 
 
 
